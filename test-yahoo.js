38b39f13585f42340dc873c9d27b02fc4b715c2d
Alen Hovhannisians <alen@robiai.com>
2026-03-20 13:15:13 +0400

    Restart server for langgraph agent follow-up

--- test-yahoo.js
+++ test-yahoo.js
@@ -1,21 +1,2 @@
-
-const { YahooFinance } = require('yahoo-finance2');
-const yahooFinance = new YahooFinance();
-
-async function testYahooFinance() {
-  try {
-    const quote = await yahooFinance.quote('AAPL');
-    console.log('AAPL Quote:', quote);
-  } catch (error) {
-    console.error('Error fetching AAPL quote:', error);
-  }
-
-  try {
-    const quote = await yahooFinance.quote('INVALID_SYMBOL');
-    console.log('INVALID_SYMBOL Quote:', quote);
-  } catch (error) {
-    console.error('Error fetching INVALID_SYMBOL quote (expected error):', error.message);
-  }
-}
-
-testYahooFinance();
+const yahooFinance = require('yahoo-finance2').default;
+yahooFinance.quote('AAPL').then(console.log).catch(console.error);
